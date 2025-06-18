5432a0e980f8e878475db894b0e4eb86d20d22b0
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2025-06-17 22:22:29 -0400

    removing other examples non microfrontend related

--- state/@still/component/manager/timer.js
+++
@@ -1,7 +0,0 @@
-export const sleepForSec = (sec) => {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(null), sec);
-    });
-}
-
-window.sleepForSec = sleepForSec;
--- todo-app/app/util/Calculation.js
+++
@@ -1,7 +0,0 @@
-export class Calculation {
-
-    sum(v1, v2){
-        console.log(v1 + v2);
-    }
-
-}
